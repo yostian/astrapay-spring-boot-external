a0a44a20f2ae57838ebd968732083f595f73966b
ap-baginda-s-hidayat <baginda.s.hidayat@astrapay.com>
2025-01-28 17:31:01 +0700

    first commit

---
+++ src/main/java/org/example/AstrapayStarterSimplified.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstrapayStarterSimplified {
+    public static void main(String[] args) {
+        SpringApplication.run(AstrapayStarterSimplified.class, args);
+    }
+}
